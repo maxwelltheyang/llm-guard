7cf41a230803484da1ecd46ede46b456bb529ff5
Maxwell Yang <maxwellbyang04@gmail.com>
2025-11-08 16:29:35 -0600

    initial results

--- codeql-db/src/C_/Users/Owner/Desktop/llm-guard/baseline.py
+++ codeql-db/src/C_/Users/Owner/Desktop/llm-guard/baseline.py
@@ -20,15 +20,6 @@ def baseline(prompt):
     return response['message']['content']
 
 if __name__ == '__main__':
-    with open('prompts/llm_multiturn_vulnerability_prompts.json', 'r') as f:
+    with open('prompts/prompts.json', 'r') as f:
         prompts = json.load(f)
-
-    results = {}
-    for i, prompt_data in enumerate(prompts):
-        prompt_copy = prompt_data.copy()
-        prompt_result = baseline(prompt_copy["Prompt"])
-        prompt_copy["Result"] = prompt_result
-        results[i] = prompt_copy
-
-    with open('results/llama3_baseline_multiturn_results.json', 'w') as f:
-        json.dump(results, f, indent=4)
+    print(baseline(prompts["1"]))
